ab10727226a0c56d370b18f77c806b68917e3d69
Tobias Reich <tobias.reich.ich@gmail.com>
2017-05-18 15:19:29 +0200

    Store currentTab and currentComponent

--- src/utils/createStore.js
+++ src/utils/createStore.js
@@ -1,8 +1,29 @@
 'use strict'
 
-const { createStore } = require('redux')
+const { compose, createStore } = require('redux')
+const { persistStore, autoRehydrate } = require('redux-persist')
 
 const reducers = require('../reducers')
 
+// Enhance the store with third-party capabilities
+const enhancers = compose(
+	autoRehydrate()
+)
+
+// Reducers to persist
+const whitelist = [
+	'currentComponent',
+	'currentTab'
+]
+
 // Create the store
-module.exports = (state) => createStore(reducers, state)
+module.exports = (state) => {
+
+	const store = createStore(reducers, state, enhancers)
+
+	// Begin periodically persisting the store
+	persistStore(store, { whitelist })
+
+	return store
+
+}
