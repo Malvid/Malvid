4bac058035ab29edcdfdf1b53cb097251f679550
Tobias Reich <tobias.reich.ich@gmail.com>
2017-06-23 11:00:17 +0200

    Fixed "bindActionCreators expected a function actionCreator"

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,10 @@
 'use strict'
 
-const {
-	SET_COMPONENT_DATA,
-	SET_ROUTE,
-	SET_CURRENT_SIZE_STATUS,
-	SET_CURRENT_SIZE_VERTICAL,
-	SET_CURRENT_SIZE_HORIZONTAL
-} = require('../constants/actions')
-
 const { setComponentData } = require('./components')
 const { setRoute } = require('./route')
 const { setCurrentSizeStatus, setCurrentSizeVertical, setCurrentSizeHorizontal } = require('./currentSize')
 
 module.exports = {
-	SET_COMPONENT_DATA,
-	SET_ROUTE,
-	SET_CURRENT_SIZE_STATUS,
-	SET_CURRENT_SIZE_VERTICAL,
-	SET_CURRENT_SIZE_HORIZONTAL,
 	setComponentData,
 	setRoute,
 	setCurrentSizeStatus,
