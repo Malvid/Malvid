8b7922de9cd9b6c001f2ac673b9379cc5f19021f
Tobias Reich <tobias.reich.ich@gmail.com>
2017-07-21 17:32:27 +0200

    Use async

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const pify = require('pify')
 const componentsLookup = require('components-lookup')
 const server = require('./server')
 
-const js = (() => {
+const clientJS = (() => {
 
 	const filePath = path.resolve(__dirname, './client.js')
 	const babel = { babelrc: false }
@@ -24,114 +24,89 @@ const js = (() => {
  * @param {?Object} opts - Options.
  * @returns {Promise} Returns the following properties if resolved: {String}.
  */
-module.exports = function(filePath, opts = {}) {
+module.exports = async function(filePath, opts = {}) {
 
-	return Promise.resolve().then(() => {
+	if (isPlainObj(opts)===false && opts!=null) throw new Error(`'opts' must be an object, null or undefined`)
 
-		if (isPlainObj(opts)===false && opts!=null) {
-			throw new Error(`'opts' must be an object, null or undefined`)
+	const resolvers = [
+		{
+			id: 'view',
+			label: 'View',
+			languages: [ 'twig' ],
+			resolve: (fileName, fileExt) => [ `${ fileName }${ fileExt }` ]
+		},
+		{
+			id: 'data',
+			label: 'Data',
+			languages: [ 'json', 'js' ],
+			resolve: (fileName, fileExt) => [ `${ fileName }.data.json`, `${ fileName }.data.js` ]
+		},
+		{
+			id: 'notes',
+			label: 'Notes',
+			languages: [ 'markdown' ],
+			resolve: (fileName, fileExt) => [ `${ fileName }.md` ]
+		},
+		{
+			id: 'config',
+			label: 'Config',
+			languages: [ 'json' ],
+			parse: (contents) => JSON.parse(contents),
+			resolve: (fileName, fileExt) => [ `${ fileName }.config.json` ]
 		}
+	]
 
-		const resolvers = [
-			{
-				id: 'view',
-				label: 'View',
-				languages: [ 'twig' ],
-				resolve: (fileName, fileExt) => [ `${ fileName }${ fileExt }` ]
-			},
-			{
-				id: 'data',
-				label: 'Data',
-				languages: [ 'json', 'js' ],
-				resolve: (fileName, fileExt) => [ `${ fileName }.data.json`, `${ fileName }.data.js` ]
-			},
-			{
-				id: 'notes',
-				label: 'Notes',
-				languages: [ 'markdown' ],
-				resolve: (fileName, fileExt) => [ `${ fileName }.md` ]
-			},
-			{
-				id: 'config',
-				label: 'Config',
-				languages: [ 'json' ],
-				parse: (contents) => JSON.parse(contents),
-				resolve: (fileName, fileExt) => [ `${ fileName }.config.json` ]
-			}
-		]
-
-		const statuses = {
-			wip: {
-				label: 'WIP',
-				description: 'Work in progress',
-				color: '#fe913d'
-			},
-			pending: {
-				label: 'Pending',
-				description: 'Ready, but waiting for finalization',
-				color: '#2d90e8'
-			},
-			ready: {
-				label: 'Ready',
-				description: 'Ready to implement',
-				color: '#2bc052'
-			}
+	const statuses = {
+		wip: {
+			label: 'WIP',
+			description: 'Work in progress',
+			color: '#fe913d'
+		},
+		pending: {
+			label: 'Pending',
+			description: 'Ready, but waiting for finalization',
+			color: '#2d90e8'
+		},
+		ready: {
+			label: 'Ready',
+			description: 'Ready to implement',
+			color: '#2bc052'
 		}
+	}
 
-		opts = Object.assign({
-			lang: 'en',
-			title: 'Rosid',
-			description: 'UI to help you build and document web components.',
-			src: '',
-			pattern: '**/[^_]*.{ejs,njk}',
-			resolvers,
-			statuses
-		}, opts)
+	opts = Object.assign({
+		lang: 'en',
+		title: 'Rosid',
+		description: 'UI to help you build and document web components.',
+		src: '',
+		pattern: '**/[^_]*.{ejs,njk}',
+		resolvers,
+		statuses
+	}, opts)
 
-	}).then(() => {
+	const components = componentsLookup(opts.pattern, opts.resolvers, { cwd: opts.src })
+	const js = await clientJS
 
-		// Get the components data
-		return componentsLookup(opts.pattern, opts.resolvers, {
-			cwd: opts.src
+	components.forEach((component) => {
+
+		// Add output to each components. This placeholder will be filled by the UI,
+		// but must exist too show up in the inspector.
+		component.data.push({
+			index: component.data.length,
+			id: 'output',
+			label: 'Output',
+			languages: [ 'html' ],
+			data: null
 		})
-
-	}).then((components) => {
-
-		// Use transformed js file and pass both js and components to next promise
-		return js.then((js) => ({
-			js,
-			components
-		}))
-
-	}).then(({ js, components }) => {
-
-		components.forEach((component) => {
-
-			// Add output to each components. This placeholder will be filled by the UI,
-			// but must exist too show up in the inspector.
-			component.data.push({
-				index: component.data.length,
-				id: 'output',
-				label: 'Output',
-				languages: [ 'html' ],
-				data: null
-			})
-
-		})
-
-		// Initial state of the site
-		const initalState = {
-			components,
-			opts
-		}
-
-		// Render the page
-		return pify(server)(initalState, js)
-
-	}).then((str) => {
-
-		return str
 
 	})
+
+	const initalState = {
+		components,
+		opts
+	}
+
+	// Render the page
+	return pify(server)(initalState, js)
 
 }
