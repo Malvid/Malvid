761a95e561e0b9d8288f1dd2769728d6fda73a51
Tobias Reich <tobias.reich.ich@gmail.com>
2017-07-31 10:46:23 +0200

    New test

---
+++ test/styles/selectable.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const assert = require('chai').assert
+const selectable = require('../../src/styles/selectable')
+
+describe('selectable', function() {
+
+	it('should be an object', function() {
+
+		assert.isObject(selectable)
+
+	})
+
+})
