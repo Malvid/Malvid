b618c2556fd6c7c3739e19cf9477a4b8c33a7387
Tobias Reich <tobias.reich.ich@gmail.com>
2017-08-03 14:04:57 +0200

    Fixed search position in Firefox

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -25,10 +25,10 @@ const style = {
 	input: css({
 		position: 'absolute',
 		top: '0',
-		right: '0',
-		bottom: '0',
 		left: '0',
 		padding: '0 0 0 2.6em',
+		width: '100%',
+		height: '100%',
 		background: 'transparent',
 		color: 'currentcolor',
 		outline: 'none',
