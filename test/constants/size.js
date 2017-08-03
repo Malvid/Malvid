bd2cb4bf51897a50e620de43bd20e7a837b27d24
Tobias Reich <tobias.reich.ich@gmail.com>
2017-08-03 10:48:00 +0200

    Fixed test using wrong file

---
+++ test/constants/size.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const assert = require('chai').assert
+const size = require('../../src/constants/size')
+
+describe('size', function() {
+
+	it('should be an object', function() {
+
+		assert.isObject(size)
+
+	})
+
+})
