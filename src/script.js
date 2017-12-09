8c3b02298fc82b4c34860e9ba4e41db0f0895d09
Tobias Reich <tobias.reich.ich@gmail.com>
2017-12-09 16:27:30 +0100

    Reduced size of generated JS files

--- src/script.js
+++ src/script.js
@@ -6,8 +6,9 @@ const handler = require('rosid-handler-js')
 module.exports = (() => {
 
 	const filePath = path.resolve(__dirname, './client.js')
+	const browserify = {}
 	const babel = { babelrc: false, compact: false }
-	const opts = { optimize: false, babel }
+	const opts = { optimize: false, browserify, babel }
 
 	return handler(filePath, opts)
 
