9404126c7c2620870ecf31ff2c1127717b385849
Tobias Reich <tobias.reich.ich@gmail.com>
2019-03-31 21:07:17 +0200

    Dynamic require ain't working

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -1,31 +1,25 @@
 const highlight = require('highlight.js/lib/highlight')
 
-const languages = [
-	'php',
-	'python',
-	'css',
-	'coffeescript',
-	'reasonml',
-	'xml',
-	'json',
-	'javascript',
-	'markdown',
-	'django',
-	'elm',
-	'htmlbars',
-	'haml',
-	'handlebars',
-	'less',
-	'scss',
-	'stylus',
-	'twig',
-	'typescript',
-	'yaml',
-	'plaintext'
-]
-
-languages.forEach((language) => {
-	highlight.registerLanguage(language, require(`highlight.js/lib/languages/${ language }`))
-})
+highlight.registerLanguage('php', require('highlight.js/lib/languages/php'))
+highlight.registerLanguage('python', require('highlight.js/lib/languages/python'))
+highlight.registerLanguage('css', require('highlight.js/lib/languages/css'))
+highlight.registerLanguage('coffeescript', require('highlight.js/lib/languages/coffeescript'))
+highlight.registerLanguage('reasonml', require('highlight.js/lib/languages/reasonml'))
+highlight.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+highlight.registerLanguage('json', require('highlight.js/lib/languages/json'))
+highlight.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+highlight.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+highlight.registerLanguage('django', require('highlight.js/lib/languages/django'))
+highlight.registerLanguage('elm', require('highlight.js/lib/languages/elm'))
+highlight.registerLanguage('htmlbars', require('highlight.js/lib/languages/htmlbars'))
+highlight.registerLanguage('haml', require('highlight.js/lib/languages/haml'))
+highlight.registerLanguage('handlebars', require('highlight.js/lib/languages/handlebars'))
+highlight.registerLanguage('less', require('highlight.js/lib/languages/less'))
+highlight.registerLanguage('scss', require('highlight.js/lib/languages/scss'))
+highlight.registerLanguage('stylus', require('highlight.js/lib/languages/stylus'))
+highlight.registerLanguage('twig', require('highlight.js/lib/languages/twig'))
+highlight.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'))
+highlight.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+highlight.registerLanguage('plaintext', require('highlight.js/lib/languages/plaintext'))
 
 module.exports = highlight
