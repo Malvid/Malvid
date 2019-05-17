f0e304635f835ed09a8e2961e4411affa93f43ea
Louis Schuhmacher <lschuhmacher@comwrap.com>
2019-05-17 15:03:54 +0200

    Added option to disable Preview for components via config.js

---
+++ src/selectors/getPreview.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const getTab = require('./getTab')
+
+module.exports = (component) => {
+
+	const config = getTab(component, 'config')
+
+	// The config is optional
+	if (config == null) return true
+	if (config.data == null) return true
+
+	const previewStatus = config.data.preview
+
+	// The group is optional
+	if (previewStatus == null) return true
+
+	return previewStatus
+
+}
